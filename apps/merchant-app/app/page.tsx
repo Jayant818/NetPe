d25239ab0bab52ba4116d051fade91b3984b37a7
Jayant <jayant@errsole.com>
2024-05-12 23:20:19 +0530

    added build workflow

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,12 +1,3 @@
-import { Button } from "@repo/ui/button";
-
 export default function Home() {
-	return (
-		<div className="text-4xl">
-			Hi There{" "}
-			<Button className="text-4xl" appName="Merchant">
-				Hello Bhaiyo
-			</Button>
-		</div>
-	);
+	return <div className="text-4xl">Hi There</div>;
 }
