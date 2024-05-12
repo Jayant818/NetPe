397e04d9b9c403d386abf98ae88ff413bdb80e43
Jayant <jayant@errsole.com>
2024-05-13 01:45:21 +0530

    ..

--- apps/user-app/app/(dashboard)/transfer/page.tsx
+++ apps/user-app/app/(dashboard)/transfer/page.tsx
@@ -43,7 +43,7 @@ const page = async () => {
 	return (
 		<div className="w-screen">
 			<div className="text-4xl text-[#6a51a6] pt-8 mb-8 font-bold">
-				Transfer
+				Transfer Money
 			</div>
 			<div className="grid grid-cols-1 gap-4 md:grid-cols-2 p-4">
 				<div>
